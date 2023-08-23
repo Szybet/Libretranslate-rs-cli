290129ef10a17c18a7d0eed35d2b018581ed03e0
Szybet <53944559+Szybet@users.noreply.github.com>
2023-08-23 21:39:11 +0200

    done?

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#rm -rf lib
+#mkdir lib
+#mkdir lib/include
+
+cargo install cross
+RUSTFLAGS='-C link-arg=-s' ~/.cargo/bin/cross build --release --target armv7-unknown-linux-musleabihf
+mv target/armv7-unknown-linux-musleabihf/release/Libretranslate-rs-to-cpp.a lib/
+
+cargo install cbindgen
+~/.cargo/bin/cbindgen . -o lib/include/libreader-rs.h
